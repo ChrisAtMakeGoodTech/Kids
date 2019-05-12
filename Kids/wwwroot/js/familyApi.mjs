34753fa678ffa30c5ab856a02d55a25615c3d8d4
ChrisAtMakeGoodTech <makegoodtech@gmail.com>
2019-05-12 02:19:15 -0500

    fixed js getFamily()

--- Kids/wwwroot/js/familyApi.mjs
+++ Kids/wwwroot/js/familyApi.mjs
@@ -1,6 +1,6 @@
 export async function getFamily() {
-	const res = await fetch('/api/family/', {
-		method: "POST", // *GET, POST, PUT, DELETE, etc.
+	const res = await fetch('/api/family/1', {
+		method: "GET", // *GET, POST, PUT, DELETE, etc.
 		mode: "cors", // no-cors, cors, *same-origin
 		cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
 		credentials: "same-origin", // include, *same-origin, omit
@@ -9,9 +9,6 @@ export async function getFamily() {
 		},
 		redirect: "follow", // manual, *follow, error
 		referrer: "no-referrer", // no-referrer, *client
-		body: JSON.stringify({
-			family: 1
-		}), // body data type must match "Content-Type" header
 	});
 	const family = await res.json();
 	return family;
